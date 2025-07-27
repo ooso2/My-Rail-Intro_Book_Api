6f8fe174bd78f91c4aca03335e64d253eaa9b002
ooso2 <ooso2@academic.rrc.ca>
2025-07-26 22:16:09 -0400

    my new commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,18 @@
 Rails.application.routes.draw do
-  get "search/index"
-  get "pages/about"
-  get "reviews/index"
-  get "reviews/show"
-  get "subjects/index"
-  get "subjects/show"
-  get "authors/index"
-  get "authors/show"
-  get "books/index"
-  get "books/show"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'books#index'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  resources :books do
+    resources :reviews, only: [:show, :index]
+  end
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  resources :authors do
+    resources :books, only: [:index]
+  end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :subjects do
+    resources :books, only: [:index]
+  end
+
+  get 'about', to: 'pages#about'
+  get 'search', to: 'search#index'
 end
